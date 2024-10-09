f879fa35d6de411342d8835333d480a848de1d38
RDoerfel <doerfelruben@aol.com>
2024-10-09 14:34:07 +0200

    [ENH] add module cli to allow to run commands from the command line. Add the entry point to the pyproject.toml file.

---
+++ refregion/cli/ref_cerebellum.py
@@ -0,0 +1,18 @@
+import argparse
+from pathlib import Path
+
+from refregion.cerebellum import cerebellum_reference_region
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a reference region for cerebellum")
+    parser.add_argument("--cerebellum", "-c", type=Path, required=True, help="Path to the cerebellum segmentation file")
+    parser.add_argument("--brain", "-b", type=Path, required=True, help="Path to the brain segmentation file")
+    parser.add_argument("--output", "-o", type=Path, required=True, help="Path to the output reference region file")
+    args = parser.parse_args()
+
+    cerebellum_reference_region(args.cerebellum_segmentation_file, args.brain_segmentation, args.output_reference_region)
+
+
+if __name__ == "__main__":
+    main()
