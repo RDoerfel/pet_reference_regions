d063a739b4b2e1b8ce83239cae34188555527cb9
RDoerfel <doerfelruben@aol.com>
2024-10-27 13:35:01 +0100

    [DOC] add docstrings to all functions in morphology module

--- refregion/morphology.py
+++ refregion/morphology.py
@@ -7,10 +7,24 @@ def _clip(mask: np.ndarray, between: tuple = (0, 1)) -> np.ndarray:
 
 
 def dialate(mask: np.ndarray, by_radius: int) -> np.ndarray:
+    """Dialate a 3D binary mask by a given radius (using a ball structuring element)
+    Args:
+        mask (np.ndarray): 3D binary mask
+        by_radius (int): Radius to dialate the mask by
+    Returns:
+        np.ndarray: Dialated mask"""
+
     dialated_mask = morphology.binary_dilation(mask, morphology.ball(by_radius))
     return _clip(dialated_mask)
 
 
 def erode(mask: np.ndarray, by_radius: int) -> np.ndarray:
+    """Erode a 3D binary mask by a given radius (using a ball structuring element)
+    Args:
+        mask (np.ndarray): 3D binary mask
+        by_radius (int): Radius to erode the mask by
+    Returns:
+        np.ndarray: Eroded mask"""
+
     eroded_mask = morphology.binary_erosion(mask, morphology.ball(by_radius))
     return _clip(eroded_mask)
