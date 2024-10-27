6cd4cc89f093fe2416396a284d13689a8484a89c
RDoerfel <doerfelruben@aol.com>
2024-10-27 13:35:13 +0100

    [ENH] add unit tests

---
+++ tests/test_morphology.py
@@ -0,0 +1,54 @@
+import numpy as np
+from refregion.morphology import dialate, erode
+
+
+def test_dialate():
+    # Create a simple binary 3D mask
+    mask = np.array(
+        [
+            [[0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
+            [[0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
+            [[0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
+        ]
+    )
+
+    # Expected result after dilation with size 1
+    expected_result = np.array(
+        [
+            [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]],
+            [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]],
+            [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]],
+        ]
+    )
+
+    # Perform dilation
+    result = dialate(mask, 1)
+
+    # Assert the result is as expected
+    assert np.array_equal(result, expected_result), "Dialate function failed"
+
+
+def test_erode():
+    # Create a simple binary mask
+    mask = np.array(
+        [
+            [[0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
+            [[0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
+            [[0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
+        ]
+    )
+
+    # Expected result after erosion with size 1
+    expected_result = np.array(
+        [
+            [[0, 0, 1, 0, 0], [0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0], [0, 0, 1, 0, 0]],
+            [[0, 0, 1, 0, 0], [0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0], [0, 0, 1, 0, 0]],
+            [[0, 0, 1, 0, 0], [0, 1, 1, 1, 0], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0], [0, 0, 1, 0, 0]],
+        ]
+    )
+
+    # Perform erosion
+    result = erode(mask, 1)
+
+    # Assert the result is as expected
+    assert np.array_equal(result, expected_result), "Erode function failed"
