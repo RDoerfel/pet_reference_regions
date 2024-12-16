86b7b996f41761cab00a91137294b66e818f5ff1
RDoerfel <doerfelruben@aol.com>
2024-12-16 11:53:05 +0100

    [maint] formatting

--- refregion/cli/ref_cerebellum.py
+++ refregion/cli/ref_cerebellum.py
@@ -11,7 +11,9 @@ def main():
     parser.add_argument("--output", "-o", type=Path, required=True, help="Path to the output reference region file")
     args = parser.parse_args()
 
-    cerebellum_reference_region(args.cerebellum_segmentation_file, args.brain_segmentation, args.output_reference_region)
+    cerebellum_reference_region(
+        args.cerebellum_segmentation_file, args.brain_segmentation, args.output_reference_region
+    )
 
 
 if __name__ == "__main__":
