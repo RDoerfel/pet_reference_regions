e69b072807d6758eea72af4df3f91e76a428da54
RDoerfel <doerfelruben@aol.com>
2025-05-22 15:19:27 +0200

    [FIX] fix input type for refregion cli arguments

--- refregion/cli/refregion.py
+++ refregion/cli/refregion.py
@@ -8,19 +8,28 @@ def main():
     parser = argparse.ArgumentParser(description="Create a custom reference region from a mask")
     parser.add_argument("--mask", "-m", type=Path, required=True, help="Path to the mask file")
     parser.add_argument(
-        "--ref_indices", "-r", type=Path, required=True, help="Indices to include in the reference region"
+        "--ref_indices",
+        "-r",
+        type=int,
+        nargs="+",
+        required=True,
+        help="Indices to include in the reference region (space-separated integers)",
     )
     parser.add_argument(
-        "--erode", "-e", type=int, required=True, help="Number of voxels to erode the reference region by"
-    )
-    parser.add_argument(
-        "--exclude_indices", "-x", type=Path, required=True, help="Indices to exclude from the reference region"
+        "--exclude_indices",
+        "-x",
+        type=int,
+        nargs="+",
+        required=False,
+        default=[],
+        help="Indices to exclude from the reference region (space-separated integers)",
     )
     parser.add_argument(
         "--dialate",
         "-d",
         type=int,
-        required=True,
+        required=False,
+        default=0,
         help="Number of voxels to dialate the excluded areas by. The overlap is removed from the reference region",
     )
     parser.add_argument("--output", "-o", type=Path, required=True, help="Path to the output reference region file")
