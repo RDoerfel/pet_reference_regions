64c1c6b505be6873e85b21db71c26f10a9b27f97
RDoerfel <doerfelruben@aol.com>
2025-05-26 12:51:28 +0200

    [fix] import cerebellum referegion correctly

--- refregion/wrappers.py
+++ refregion/wrappers.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import nibabel as nib
+from refregion import cerebellum
 from refregion import refregion
+
 import numpy as np
 
 
@@ -15,9 +17,7 @@ def custom_ref_region(
     # load data
     mask = nib.load(mask_file).get_fdata()
     # create custom reference region
-    mask_ref = refregion.custom_ref_region(
-        mask, refregion_indices, erode_by_voxels, exclude_indices, dialate_by_voxels
-    )
+    mask_ref = refregion.custom_ref_region(mask, refregion_indices, erode_by_voxels, exclude_indices, dialate_by_voxels)
     # cast as uint8
     mask_ref = mask_ref.astype(np.uint8)
     # save output
@@ -34,13 +34,13 @@ def cerebellum_reference_region(
     output_reference_region: Path,
 ):
     # load data
-    cerebellum = nib.load(cerebellum_segmentation_file).get_fdata()
-    brain = nib.load(brain_segmentation).get_fdata()
+    cerebellum_data = nib.load(cerebellum_segmentation_file).get_fdata()
+    brain_data = nib.load(brain_segmentation).get_fdata()
     # create cerebellum reference region
-    cerebellum_ref = refregion.cerebellum_reference_region(cerebellum, brain)
+    cerebellum_refregion = cerebellum.cerebellum_reference_region(cerebellum_data, brain_data)
     # save output
     nib.save(
-        nib.Nifti1Image(cerebellum_ref, nib.load(cerebellum_segmentation_file).affine),
+        nib.Nifti1Image(cerebellum_refregion, nib.load(cerebellum_segmentation_file).affine),
         output_reference_region,
         dtype=np.uint8,
     )
